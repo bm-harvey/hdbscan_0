4d06c1812cd8b2db7ce1ea85e37f97634ccb7c91
bm-harvey <bmharvey@live.com>
2023-05-26 10:56:00 -0700

    first commit - implemented skeleton Point and Clusterer structs.

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+use std::borrow::{Borrow};
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Point {
+    coordinate: Vec<f64>,
+    nearest_neighbors: Vec<Rc<RefCell<Point>>>,
+}
+
+impl Point {
+    // ctor
+    pub fn from(coord: Vec<f64>) -> Self {
+        Self {
+            coordinate: coord,
+            nearest_neighbors: vec![],
+        }
+    }
+
+    pub fn from_as_rcc(coord: Vec<f64>) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Point::from(coord)))
+    }
+
+
+    pub fn new() -> Self {
+        Self {
+            coordinate: vec![],
+            nearest_neighbors: vec![],
+        }
+    }
+    
+    pub fn new_as_rcc() -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Point::new()))
+    }
+
+    // access fns
+    pub fn num_dimensions(&self) -> usize {
+        self.coordinate.len()
+    }
+
+    pub fn get_coord(&self, index: usize) -> Option<&f64> {
+        return self.coordinate.get(index);
+    }
+
+pub fn distance(point_1 : Rc<RefCell<Point>> , point_2 :  Rc<RefCell<Point>>) -> f64 {
+        // if this passes, the rest of the calculation must be safe.
+        let point_1 = RefCell::borrow(&point_1);
+        let point_2 = RefCell::borrow(&point_2); 
+
+        assert_eq!(point_1.num_dimensions(), point_2.num_dimensions());
+
+        let n_dim = point_1.num_dimensions();
+        let mut sum: f64 = 0.0;
+        for index in 0..n_dim {
+            sum += (point_1.get_coord(index).unwrap() - point_2.get_coord(index).unwrap()).powf(2.);
+        }
+        sum.sqrt()
+    }
+
+    // mut
+    pub fn add_neighbor(&mut self, other: Rc<RefCell<Point>>) {
+        self.nearest_neighbors.push(Rc::clone(&other.borrow()));
+    }
+
+    pub fn scale_data(&mut self, scale: f64, offset: f64) {
+        for coord in &mut self.coordinate {
+            *coord = (*coord) * scale + offset;
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Clusterer {
+    data: Vec<Rc<RefCell<Point>>>,
+}
+
+impl Clusterer {
+    // ctor-like
+    pub fn new() -> Self {
+        Self { data: vec![]}
+    }
+    
+    pub fn from_data(d : Vec<Rc<RefCell<Point>>>) -> Self {
+        Self { data: d }
+    }
+
+    // mut
+    pub fn scale_data(&mut self, scale: f64, offset: f64) {
+        for point in &mut self.data {
+
+            let mut mut_point = RefCell::borrow_mut(point);
+            mut_point.scale_data(scale, offset);
+            // i need to use the Rc<RefCell<Point>> pattern    
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct BallTree{
+    leaf_size: u32,
+}
+
+impl BallTree {
+}
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use hdbscan_0::{self, Point, Clusterer};
+use std::{rc::Rc};
+
+fn main() {
+
+    let p_1 = Point::from_as_rcc(vec![2., 2.4]);
+    let p_2 = Point::from_as_rcc(vec![3., 4.3]);
+    let p_3 = Point::from_as_rcc(vec![5., 4.3]);
+    //let p_4 = Point::from_as_rcc(vec![2., -5.2]);
+
+    println!("the generated points are  : {:?} , {:?}, {:?}", p_1, p_2, p_3);
+
+    let mut clusterer = Clusterer::from_data(vec![Rc::clone(&p_1), Rc::clone(&p_2)]);
+
+    println!("the distance between the points is : {}", Point::distance(p_1, p_3));
+
+    println!("The clusterer pre-scaling is {:#?}", clusterer);
+    
+    clusterer.scale_data(3., 3.);
+    
+    println!("The clusterer post-scaling is {:#?}", clusterer);
+}
