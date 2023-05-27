f69502ca3b1de7514d7ee9056ebd0faf4875f78d
bm-harvey <bmharvey@live.com>
2023-05-27 08:04:18 -0700

    messy, untested, and useless ball tree constructions

--- src/lib.rs
+++ src/lib.rs
@@ -75,171 +75,11 @@ impl Default for Point {
     }
 }
 
-//
-// Ball tree
-//
-trait BallTree {
-    //    fn construct(data: Vec<Rc<RefCell<Point>>>, leaf_size: usize) -> Self;
+/*
 
-    fn is_leaf(&self) -> bool;
-}
+*/
 
-#[derive(Debug)]
-struct BallTreeBranch<L: BallTree, R: BallTree> {
-    child_left: Box<L>,
-    child_right: Box<R>,
-}
-
-struct BallTreeLeaf {
-    data: Vec<Rc<RefCell<Point>>>,
-}
-
-
-impl<L: BallTree, R: BallTree> BallTreeBranch<L, R> {
-    fn generate_ball_tree(data: Vec<Rc<RefCell<Point>>>, leaf_size: usize) -> Box<dyn BallTree> {
-        // grab any point, find the point furthest from that point, find the point furthest from the second point
-        // the second and third point are generally far from each other.
-        // todo - There might be better ways to do this in the future
-        // todo - in general this code is gross but it do the thing. clean it up sometime
-        assert!(data.len() >= 2);
-
-        let random_pnt = Rc::clone(data[0].borrow());
-
-        // todo - is there a more idiomatic way to do loops like this with closures?
-        let mut far_pnt_1 = Rc::clone(data[1].borrow());
-        let mut max_distance = Point::distance(&far_pnt_1, &random_pnt);
-        for index in 1..(data.len()) {
-            let pnt = Rc::clone(data[index].borrow());
-            let dist = Point::distance(&pnt, &random_pnt);
-            if max_distance < dist {
-                max_distance = dist;
-                far_pnt_1 = pnt;
-            }
-        }
-
-        let mut far_pnt_2 = Rc::clone(data[0].borrow());
-        let mut max_distance = Point::distance(&far_pnt_1, &far_pnt_2);
-        for index in 1..(data.len()) {
-            if index == 1 {
-                continue;
-            }
-            let pnt = Rc::clone(data[index].borrow());
-            let dist = Point::distance(&pnt, &random_pnt);
-            if max_distance < dist {
-                max_distance = dist;
-                far_pnt_2 = pnt;
-            }
-        }
-
-        let mut vec_left: Vec<Rc<RefCell<Point>>> = vec![];
-        let mut vec_right: Vec<Rc<RefCell<Point>>> = vec![];
-
-        for point_ref in &data {
-            if Point::distance(&far_pnt_1, &point_ref) < Point::distance(&far_pnt_2, &point_ref) {
-                vec_left.push(Rc::clone(point_ref));
-            } else {
-                vec_right.push(Rc::clone(point_ref));
-            }
-        }
-
-        if vec_left.len() > leaf_size && vec_right.len() > leaf_size {
-        let child_l = BallTreeBranch::generate_ball_tree(vec_left, leaf_size);
-        let child_r = BallTreeBranch::generate_ball_tree(vec_right, leaf_size);
-
-            return Box::new(
-                BallTreeBranch {
-                    child_left: child_l,
-                    child_right:child_2
-                }
-            );
-        } else {
-            return Box::new(BallTreeLeaf { data });
-        }
-    }
-}
-
-impl<L: BallTree, R: BallTree> BallTree for BallTreeBranch<L, R> {
-    fn is_leaf(&self) -> bool {
-        false
-    }
-    /*
-    fn construct(data: Vec<Rc<RefCell<Point>>>, leaf_size: usize) -> Self {
-        // grab any point, find the point furthest from that point, find the point furthest from the second point
-        // the second and third point are generally far from each other.
-        // todo - There might be better ways to do this in the future
-        // todo - in general this code is gross but it do the thing. clean it up sometime
-        assert!(data.len() >= 2);
-
-        let random_pnt = Rc::clone(data[0].borrow());
-
-        // todo - is there a more idiomatic way to do loops like this with closures?
-        let mut far_pnt_1 = Rc::clone(data[1].borrow());
-        let mut max_distance = Point::distance(&far_pnt_1, &random_pnt);
-        for index in 1..(data.len()) {
-            let pnt = Rc::clone(data[index].borrow());
-            let dist = Point::distance(&pnt, &random_pnt);
-            if max_distance < dist {
-                max_distance = dist;
-                far_pnt_1 = pnt;
-            }
-        }
-
-        let mut far_pnt_2 = Rc::clone(data[0].borrow());
-        let mut max_distance = Point::distance(&far_pnt_1, &far_pnt_2);
-        for index in 1..(data.len()) {
-            if index == 1 {
-                continue;
-            }
-            let pnt = Rc::clone(data[index].borrow());
-            let dist = Point::distance(&pnt, &random_pnt);
-            if max_distance < dist {
-                max_distance = dist;
-                far_pnt_2 = pnt;
-            }
-        }
-
-        let mut vec_left: Vec<Rc<RefCell<Point>>> = vec![];
-        let mut vec_right: Vec<Rc<RefCell<Point>>> = vec![];
-
-        for point_ref in &data {
-            if Point::distance(&far_pnt_1, &point_ref) < Point::distance(&far_pnt_2, &point_ref) {
-                vec_left.push(Rc::clone(point_ref));
-            } else {
-                vec_right.push(Rc::clone(point_ref));
-            }
-        }
-
-        if vec_left.len() < leaf_size {
-            return BallTreeBranch {
-                child_left: Box::new(BallTreeLeaf::construct(data, leaf_size)),
-                child_right: Box::new(BallTreeLeaf::construct(data, leaf_size)),
-            };
-        }
-
-        Self {
-            child_left: Box::new(L::construct(data, leaf_size)),
-            child_right: Box::new(R::new()),
-        }
-    }
-    */
-}
-
-impl BallTreeLeaf {}
-
-impl BallTree for BallTreeLeaf {
-    fn is_leaf(&self) -> bool {
-        true
-    }
-
-    // fn construct(d: Vec<Rc<RefCell<Point>>>, _leaf_size: usize) -> Self {
-    //     Self { data: d }
-    // }
-}
-//
-// Clusterer
-//
-#[derive(Debug)]
-pub struct Clusterer {
+struct Clusterer {
     data: Vec<Rc<RefCell<Point>>>,
 }
 
@@ -267,4 +107,79 @@ impl Default for Clusterer {
     fn default() -> Self {
         Self::new()
     }
+}
+
+struct BallTreeBranchData {
+    child_left: Box<BallTree>,
+    child_right: Box<BallTree>,
+}
+
+impl BallTreeBranchData {
+    fn construct(data: Vec<Rc<RefCell<Point>>>, leaf_size: usize) -> Box<BallTree> {
+        // grab any point, find the point furthest from that point, find the point furthest from the second point
+        // the second and third point are generally far from each other.
+        // todo - There might be better ways to do this in the future
+        // todo - in general this code is gross but it do the thing. clean it up sometime
+
+        if data.len() < leaf_size || data.len() < 2 {
+            return Box::new(BallTree::BallTreeLeaf(BallTreeLeafData { data }));
+        }
+
+        let random_pnt = Rc::clone(data[0].borrow());
+
+        // todo - is there a more idiomatic way to do loops like this with closures?
+        let mut far_pnt_1 = Rc::clone(data[1].borrow());
+        let mut max_distance = Point::distance(&far_pnt_1, &random_pnt);
+        for index in 1..(data.len()) {
+            let pnt = Rc::clone(data[index].borrow());
+            let dist = Point::distance(&pnt, &random_pnt);
+            if max_distance < dist {
+                max_distance = dist;
+                far_pnt_1 = pnt;
+            }
+        }
+
+        let mut far_pnt_2 = Rc::clone(data[0].borrow());
+        let mut max_distance = Point::distance(&far_pnt_1, &far_pnt_2);
+        for index in 1..(data.len()) {
+            if index == 1 {
+                continue;
+            }
+            let pnt = Rc::clone(data[index].borrow());
+            let dist = Point::distance(&pnt, &random_pnt);
+            if max_distance < dist {
+                max_distance = dist;
+                far_pnt_2 = pnt;
+            }
+        }
+
+        let mut vec_left: Vec<Rc<RefCell<Point>>> = vec![];
+        let mut vec_right: Vec<Rc<RefCell<Point>>> = vec![];
+
+        for point_ref in &data {
+            if Point::distance(&far_pnt_1, &point_ref) < Point::distance(&far_pnt_2, &point_ref) {
+                vec_left.push(Rc::clone(point_ref));
+            } else {
+                vec_right.push(Rc::clone(point_ref));
+            }
+        }
+
+        let child_left = BallTreeBranchData::construct(vec_left, leaf_size);
+        let child_right = BallTreeBranchData::construct(vec_right, leaf_size);
+
+        return Box::new(BallTree::BallTreeBranch(BallTreeBranchData {
+            child_left,
+            child_right,
+        }));
+
+        //s        return BallTree(BallTreeLeafData{data});
+    }
+}
+struct BallTreeLeafData {
+    data: Vec<Rc<RefCell<Point>>>,
+}
+
+enum BallTree {
+    BallTreeBranch(BallTreeBranchData),
+    BallTreeLeaf(BallTreeLeafData),
 }
